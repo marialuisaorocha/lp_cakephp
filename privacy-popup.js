9ccaef68e53e732573c1ca6e88b5e8a7863302e5
Maria Luisa O Rocha <maahwf@gmail.com>
2021-10-20 19:03:52 -0300

    added popup

--- privacy-popup.js
+++ privacy-popup.js
@@ -1,5 +1,17 @@
-function openPopUp (popupID) {
-    const popup = document.getElementById(popupID);
-    console.log(popupID);
+function iniciaModal (modalID) {
+    const modal = document.getElementById(modalID);
+    if(modal) {
+        modal.classList.add('mostrar');
+        modal.addEventListener('click', (e) => {
+            if(e.target.id == modalID || e.target.className == 'close') {
+                modal.classList.remove('mostrar');
+            }
+        });
+    }
 }
+
+const politicaPrivacidade = document.querySelector('.politica-privacidade');
+politicaPrivacidade.addEventListener('click', () => iniciaModal('privacy-popup'));
+
+
 

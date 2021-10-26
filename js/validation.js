720f757a60a042551e8186d1b6067e8215b4aff5
Maria Luisa O Rocha <maahwf@gmail.com>
2021-10-26 19:38:50 -0300

    radio button validation

--- js/validation.js
+++ js/validation.js
@@ -1,51 +1,130 @@
+// botar label no sim e não ✓
+// pegar todos os campos e inputs do formulario ✓
+// verificar se o campo é radio, se for, verificar se ele tem outro campo com mesmo nome
+// se for o mesmo nome, verificar se algum deles está checado, se não estiver dá erro de validação
+// mexer no verifyErrors() e verificar se o campo que está vindo tem o required, se ele tiver, vai fazer a validacao normal
+// se ele não tiver o required, vai ter que fazer a regra acima
+const campos = document.querySelectorAll('input')
 
-const campos = document.querySelectorAll('[required]')
+console.log(campos);
 
-// console.log(campos)
-
-function customValidation(event) {
-    // a fazer: customizar o layout e mensagens de erros
-
-
-
-
-
-    const campo =  event.target
-
-    // verificar se existem erros
+function ValidateField(campo) {
     function verifyErrors() {
         let foundError = false;
 
-        // procurar dentro do validity algum item verdadeiro
-        for(const error in campo.validity) {
+        for(let error in campo.validity) {
             // se não for customError, verifica se tem erro
-            if (error != "customError" && campo.validity[error]) {
+            if (campo.validity[error] && !campo.validity.valid) {
                 foundError = error
             }
         }
 
+        console.log(campo)
+        if (campo.getAttribute('type') == 'radio' && campo.getAttribute('name') == 'souEmpreendedor') {
+            if (checkIsEmpreendedorRadioButton()) {
+                foundError = 'valueMissing'
+            }
+
+
+        }
         return foundError;
     }
 
-    const error = verifyErrors()
-    console.log("Error exists: ", error)
+    function customMessage(typeError) {
+        const messages = {
+            text: {
+                valueMissing: "Preencha este campo."
+            },
+            email: {
+                valueMissing: "Insira um e-mail.",
+                typeMismatch: "Preencha com um e-mail válido."
+            },
+            radio: {
+                valueMissing: "Selecione uma opção."
+            }
+        }
 
-    if (error) {
-        // trocar mensagem de required
-        campo.setCustomValidity("Este campo é obrigatório.")
-    } else {
-        campo.setCustomValidity("")
-
+        return messages[campo.type][typeError]
     }
 
+    function setCustomMessage(message) {
+
+        const spanError = campo.parentNode.querySelector("span.error")
+
+        if (message) {
+            spanError.classList.add("active")
+            spanError.innerHTML = message
+            return
+        }
+        spanError.classList.remove("active")
+        spanError.innerHTML = ""
+    }
+
+    function checkIsEmpreendedorRadioButton() {
+
+        const isEmpreendedor = document.querySelectorAll("input[type='radio'][name='souEmpreendedor']")
+
+        console.log(isEmpreendedor)
+
+        if(document.form1.souEmpreendedor[0].checked == false
+            && document.form1.souEmpreendedor[1].checked == false) {
+              return false;
+            }
+            return false;
+    }
+
+    return function() {
+        const error = verifyErrors()
+
+        if(error) {
+            const message = customMessage(error)
+
+            campo.style.borderColor = "red"
+            setCustomMessage(message)
+            return
+        }
+            campo.style.borderColor = "green"
+            setCustomMessage()
+    }
 }
+
+
+function customValidation(event) {
+
+    const campo =  event.target
+    //validateField retorna uma função
+    const validation = ValidateField(campo)
+
+    validation()
+
+}
+
 for(const campo of campos) {
-    campo.addEventListener("invalid", customValidation)
+    campo.addEventListener("invalid", event => {
+        // eliminar o bubble
+        event.preventDefault()
+
+        customValidation(event)
+    })
+    campo.addEventListener("blur", customValidation)
 }
 
 document.querySelector('form').addEventListener('submit', event => {
     console.log("enviar o formulario")
 
-    // nao vai enviar o formulario
+    const spanError = document.querySelector('form .radio-buttons .error')
+    if (!checkIsEmpreendedorRadioButton()) {
+        spanError.classList.add("active")
+        spanError.innerHTML = 'bla'
+        event.preventDefault()
+
+        console.log('falhou')
+
+        return
+    }
+    spanError.classList.remove("active")
+    spanError.innerHTML = ""
+
+    // impedir que envie os dados
     event.preventDefault()
 })
